119c8b4db429fca8f35a3e291a571ca898bde5e2
Amir Attar <amira@dropitshopping.com>
2025-04-26 18:45:18 +0300

    remove flask core

--- app/app.py
+++ app/app.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from flask_cors import CORS
 
 from .repository import db, init_db
 from .api.pokemon_controller import api
@@ -10,9 +9,6 @@ from .api.capture_controller import capture_api
 def create_app():
     """Create and configure the Flask application"""
     app = Flask(__name__)
-
-    # Enable CORS for all routes and origins
-    CORS(app)
 
     # Configure the database
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
